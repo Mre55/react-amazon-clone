63be702ca7955b31d378f1145e5603fcbe0c371a
mre55 <misganawmihreteab>
2022-05-25 15:34:50 +0300

    Add MUI and stye the nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello, amazon clone.</h1>
+      <Header />
     </div>
   );
 }
