edafcf0797c68fec3fe235fc29bd6c5f04ccf35c
mre55 <misganawmihreteab>
2022-05-25 12:57:42 +0300

    Clear unused files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Sob7AH3wH9UWYEp2qxZc645KwHqxt_w",
+  authDomain: "challenge-4d469.firebaseapp.com",
+  projectId: "challenge-4d469",
+  storageBucket: "challenge-4d469.appspot.com",
+  messagingSenderId: "13454605662",
+  appId: "1:13454605662:web:ab75cc303b567e520a63cf",
+  measurementId: "G-ZMVGCSV7R1"
+};
