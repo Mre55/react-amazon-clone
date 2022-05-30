8b74239acf493febf7801889952998631d30f8bb
mre55 <misganawmihreteab>
2022-05-30 13:11:38 +0300

    Implement Add to basket button

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, price, rating, image }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log('this is the basket', basket);
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: { id, title, price, rating, image },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -22,7 +34,7 @@ function Product({ id, title, price, rating, image }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
