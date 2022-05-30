e9edf902bf29acfb359fb180a68d90fca86fe6e6
mre55 <misganawmihreteab>
2022-05-30 13:36:45 +0300

    Write getBasketTotal function inside the reducer

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 import './Subtotal.css'
 
@@ -22,7 +23,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={basket?.reduce((acc, curr) => acc + curr.price, 0)}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
