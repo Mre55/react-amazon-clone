01162a4bf061732dfeb45e94dd8d2339688b3206
mre55 <misganawmihreteab>
2022-05-31 20:08:21 +0300

    Add form to sign in and style the content of the page

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <NavLink to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="amazon logo"
+        />
+      </NavLink>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" className="login__signInButton">
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
