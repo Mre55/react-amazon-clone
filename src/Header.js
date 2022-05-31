2b9dc62cc545c0f231238aec28e3f1d64c6e6177
mre55 <misganawmihreteab>
2022-05-31 14:42:17 +0300

    Edit Header component to redirect to Login page

--- src/Header.js
+++ src/Header.js
@@ -25,10 +25,12 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
-        </div>
+        <NavLink to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </NavLink>
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
