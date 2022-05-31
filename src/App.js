92b6054376b95b1353826f6f4a44723d533caae0
mre55 <misganawmihreteab>
2022-05-31 14:42:30 +0300

    Add route for login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/checkout" element={ <Checkout /> } />
         </Routes>
