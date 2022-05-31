580e79f5c9f3a86a502479bfc63516cf43e58215
mre55 <misganawmihreteab>
2022-06-01 00:22:30 +0300

    Handle sign in and sign out functionality for the user with firebase

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,17 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { NavLink } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
 
-  const [{ basket }] = useStateValue();
+  const [{ basket, user }] = useStateValue();
+
+  const handlAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
 
   return (
     <div className="header">
@@ -24,11 +31,11 @@ function Header() {
         <SearchIcon className="header__searchIcon" />
       </div>
 
-      <div className="header__nav">
-        <NavLink to="/login" className="header__link">
+      <div onClick={handlAuthentication} className="header__nav">
+        <NavLink to={!user && "/login"} className="header__link">
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">{ user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </NavLink>
 
