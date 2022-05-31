00b6ff2a0855d5a56c0b17875a6e7c8aa472bec7
mre55 <misganawmihreteab>
2022-05-31 13:46:04 +0300

    Add CheckoutProduct component and style it

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './CheckoutProduct.css'
+
+function CheckoutProduct({ id, title, price, rating, image }) {
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} alt="" />
+
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (  
+              <p>⭐</p>
+          ))}
+        </div>
+
+        <button>Remove from basket</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
