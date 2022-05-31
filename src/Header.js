f10703e43ff754a43f32c020d417a6d392fde62b
mre55 <misganawmihreteab>
2022-06-01 00:43:14 +0300

    Add dynamic render for name of the user in the header

--- src/Header.js
+++ src/Header.js
@@ -34,7 +34,7 @@ function Header() {
       <div onClick={handlAuthentication} className="header__nav">
         <NavLink to={!user && "/login"} className="header__link">
           <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
+            <span className="header__optionLineOne">Hello { user ? user.email : 'Guest'}</span>
             <span className="header__optionLineTwo">{ user ? 'Sign Out' : 'Sign In'}</span>
           </div>
         </NavLink>
