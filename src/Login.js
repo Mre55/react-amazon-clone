f1667f9cbca1292bd05e87a9ba62049c0750898c
mre55 <misganawmihreteab>
2022-05-31 23:33:21 +0300

    Navigate the user to homepage after creating new account

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,31 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./Login.css";
+import { auth } from "./firebase";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          navigate("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <NavLink to="/">
@@ -20,18 +43,22 @@ function Login() {
           <h5>E-mail</h5>
           <input
             type="text"
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <h5>Password</h5>
           <input
             type="password"
-            // value={password}
-            // onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <button type="submit" className="login__signInButton">
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
             Sign In
           </button>
         </form>
@@ -42,7 +69,7 @@ function Login() {
           Interest-Based Ads Notice.
         </p>
 
-        <button className="login__registerButton">
+        <button className="login__registerButton" onClick={register}>
           Create your Amazon Account
         </button>
       </div>
