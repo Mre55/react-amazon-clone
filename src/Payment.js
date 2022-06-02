825c646cb884e03f7070dd1ac09ec3598de96443
mre55 <misganawmihreteab>
2022-06-02 20:41:32 +0300

    Add Payment component

---
+++ src/Payment.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Payment.css'
+
+function Payment() {
+  return (
+    <div className='payment'>
+
+      <div className='payment__container'>
+      
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+        </div>
+        
+        <div className='payment__section'>
+        
+        </div>
+        
+        <div className='payment__section'>
+        
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Payment
