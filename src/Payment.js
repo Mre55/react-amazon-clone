d384865a3258b4a70d5bbeb943ad474545d49997
mre55 <misganawmihreteab>
2022-06-07 22:19:09 +0300

    Render the content to payment page

--- src/Payment.js
+++ src/Payment.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import './Payment.css'
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from './CheckoutProduct';
 
 function Payment() {
+
+  const [{ basket, user }] = useStateValue();
+
   return (
     <div className='payment'>
 
@@ -11,9 +16,32 @@ function Payment() {
           <div className='payment__title'>
             <h3>Delivery Address</h3>
           </div>
+
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+          
         </div>
         
         <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review Items and Delivery</h3>
+          </div>
+
+          <div className='payment__items'>
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+
         
         </div>
         
